c40c05480b9b50cf3af6a902a7e1aa6d5c640bb3
limaj-byte <lima.j@estudante.ifro.edu.br>
2026-09-24 16:46:43 -0400

    Adição de arquivos

---
+++ ArquivosPrincipais/Projetos/Desafios/At6/script.js
@@ -0,0 +1,12 @@
+function abrir(src){
+
+    document.getElementById("modal").style.display="block";
+    document.getElementById("imagemGrande").src=src;
+
+}
+
+function fechar(){
+
+    document.getElementById("modal").style.display="none";
+
+}
